8c57cd5ac05ac005f5b7443e0742ac6e68573bb7
PCollander <63548183+PCollander@users.noreply.github.com>
2022-01-03 10:55:37 +0200

    Delete webdev1-group-38-master directory

--- webdev1-group-38-master/utils/products.js
+++
@@ -1,9 +0,0 @@
-const productData = {
-  products: require('../products.json').map(product => ({ ...product })),
-};
-
-const getProducts = () => {
-  return [...productData.products];
-};
-
-module.exports = { getProducts };
